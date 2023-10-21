28c77e10de5641d3b0d694ebc762fdaa5e39ebdc
toufy <ar.toufic@protonmail.com>
2023-10-21 10:43:24 +0000

    live grep and add custom config to nvim

---
+++ .config/nvim/lua/myconfig/packer.lua
@@ -0,0 +1,39 @@
+vim.cmd [[packadd packer.nvim]]
+return require('packer').startup(function(use)
+	-- packer
+	use 'wbthomason/packer.nvim'
+	-- telescope
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.4',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+	-- catppuccin
+	use {
+		"catppuccin/nvim",
+		as = "catppuccin",
+		config = function()
+			vim.cmd('colorscheme catppuccin-mocha')
+		end
+	}
+	-- treesitter
+	use (
+	'nvim-treesitter/nvim-treesitter',
+	{run = ':TSUpdate'}
+	)
+	-- undotree
+	use 'mbbill/undotree'
+	-- lsp-zero
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v3.x',
+		requires = {
+			{'williamboman/mason.nvim'},
+			{'williamboman/mason-lspconfig.nvim'},
+			{'neovim/nvim-lspconfig'},
+			{'hrsh7th/nvim-cmp'},
+			{'hrsh7th/cmp-nvim-lsp'},
+			{'L3MON4D3/LuaSnip'},
+		}
+	}
+end
+)
---
+++ .config/nvim/lua/myconfig/remaps.lua
@@ -0,0 +1,4 @@
+-- space as leader
+vim.g.mapleader = " "
+-- explorer with ` cd`
+vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
