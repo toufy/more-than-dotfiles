57f5cc6056227b1e7a5a2afffeaea780ac37bad8
toufy <ar.toufic@protonmail.com>
2023-10-21 10:52:39 +0000

    safe environment.d

--- .bash/aliases.sh
+++ .bash/aliases.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 alias aliases="less ~/.bash/aliases.sh"
-alias ls="exa -la --group-directories-first --icons --color always --classify"
+alias ls="exa -la --group-directories-first --color always --classify"
 alias cat="bat"
 alias termcolors='$HOME/.bash/scripts/termcols.sh'
 alias yay="paru"
